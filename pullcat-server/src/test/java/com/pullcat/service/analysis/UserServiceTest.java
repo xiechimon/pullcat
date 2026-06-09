366a524777ca7c077d966a73b5a3a987aed04d36
xiechimon <xiechimon@qq.com>
2026-06-09 17:07:43 +0800

    refactor(test): 补充 mock 依赖

--- pullcat-server/src/test/java/com/pullcat/service/analysis/UserServiceTest.java
+++ pullcat-server/src/test/java/com/pullcat/service/analysis/UserServiceTest.java
@@ -16,8 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
